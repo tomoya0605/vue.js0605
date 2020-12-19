7063d9efd6034d0dc8cbe38a7ed076efbfe7718f
tomoya0605 <t.tomoya0605@gmail.com>
2020-12-20 08:47:19 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,29 @@
 <template>
-  <LikeNumber></LikeNumber>
+  <div>
+    <LikeHeader></LikeHeader>
+    <p>{{ number }}</p>
+    <LikeNumber :total-number="number" @my-click="number = $event"></LikeNumber>
+    <LikeNumber :total-number="number" ></LikeNumber>
+  </div>
 </template>
+
+<script>
+import LikeHeader from './components/LikeHeader.vue';
+
+export default {
+  data() {
+    return {
+      number: 14
+    }
+  },
+  components: {
+    LikeHeader
+  }
+};
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid blue;
+  }
+</style>
