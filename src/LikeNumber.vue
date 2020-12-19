eaf752cf24bbdca9d9075349b9c0a18522ba67ab
tomoya0605 <t.tomoya0605@gmail.com>
2020-12-20 05:52:47 +0900

    firstcommit

---
+++ src/LikeNumber.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>いいね({{ number }})</p>
+  <botton @click="increment">+1</botton>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      number:5
+    };
+  },
+  methods: {
+    increment() {
+      this.number +=1;
+    }
+  }
+};
+</script>
