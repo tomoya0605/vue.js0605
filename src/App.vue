2d980cb9a3735767f79ceb3b6ac2d8987e1b9daf
tomoya0605 <t.tomoya0605@gmail.com>
2020-12-21 06:57:08 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,34 @@
 <template>
   <div>
     <LikeHeader>
-        <h2>みなさん</h2>
-      <template v-slot:title >
-        <h2>こんにちわ</h2>
-      </template>
         <h3>はじめまして</h3>
-        <p>よろしくお願いします</p>
-      <template v-slot:number>
-        <p>{{ number }}</p>
-      </template>
     </LikeHeader>
     <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
-    <LikeNumber :total-number="number" ></LikeNumber>
-
+    <button @click="currentComponent = 'Home' ">Home</button>
+    <button @click="currentComponent = 'About' ">About</button>
+    <component :is="currentComponent"></component>
+    <keep-alive>
+      
+    </keep-alive>
   </div>
 </template>
 
 <script>
 import LikeHeader from './components/LikeHeader.vue';
+import About from './components/About.vue';
+import Home from './components/Home.vue';
 
 export default {
   data() {
     return {
-      number: 14
-    }
+      number: 14,
+      currentComponent: "Home"
+    };
   },
   components: {
-    LikeHeader
+    LikeHeader,
+    About,
+    Home,
   },
   methods: {
     incrementNumber(value) {
